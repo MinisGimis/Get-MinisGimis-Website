f03971c2eebe111fc3d1507bc3f9c951a40dc77d
MinisGimis <81587461+MinisGimis@users.noreply.github.com>
2021-09-24 01:31:44 -0400

    fixed notification visual bug, did some balancing to events

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 
 function App() {
 
-  const [minisgimis, setminisgimis] = useState(() => {
+  const [logs, setlogs] = useState(() => {
     return 0
   })
 
@@ -11,71 +11,92 @@ function App() {
     return 1
   })
 
+  const [time, setTime] = useState(() => {
+    return 0
+  })
+
+
+
   function minisgimisIt() {
-    setminisgimis(prevminisgimis => prevminisgimis + minisgimisPower)
+    setlogs(prevlog => prevlog + minisgimisPower)
   }
 
   function increasePower() {
     var notification_bar = document.getElementById("notification")
     var required = Math.round(((minisgimisPower*minisgimisPower)*0.75) + 10)
-    if (minisgimis < required) {
+    if (logs < required) {
       
       notification_bar.innerHTML = (`not enough MinisGimis, need ${required} MinisGimis!`.bold())
-      setTimeout(() => { notification_bar.innerHTML=("Made by minisgimis#3308")}, 3000);
+      setTime(3)
+      
     }
     else {
-      setminisgimis(prevminisgimis => prevminisgimis - required)
+      setlogs(prevlogs => prevlogs - required)
       setminisgimisPower(prevPower => prevPower + 1)
       notification_bar.innerHTML = (`MinisGimis' strength increased to ${minisgimisPower + 1}!`.bold())
-      setTimeout(() => { notification_bar.innerHTML=("Made by minisgimis#3308")}, 3000);
+      setTime(3)
+      
     }
   }
 
   function moreInformation() {
-    alert("Random website made by MinisGimis to test out React hooks and Azure Static Web Apps. You can contact me on Discord at MinisGimis#3308 or dk3zhang@uwaterloo.ca.\nClicking on Get x MinisGimis will increase your MinisGimis count, feeding MinisGimis will increase the number of MinisGimis you get each time. You may occasionally get lucky and find your MinisGimis strength increased or your MinisGimis count doubled!")
+    alert("Random website made by MinisGimis to test out React hooks and Azure Static Web Apps. You can contact me on Discord at MinisGimis#3308 or dk3zhang@uwaterloo.ca.\nClicking on Chop x Trees will give you logs, feeding MinisGimis will increase how many logs you chop. There are also bonus events giving you free logs or buffing your MinisGimis!")
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
       var lucky_num = Math.random()
       var notification_bar = document.getElementById("notification")
-      if (lucky_num < 0.015 && lucky_num > 0.009) {
-        var increase = Math.floor(Math.random()*5) + 1
-        setminisgimisPower(prevPower => prevPower + 1 + increase)
+      console.log(time)
+
+      if (time === 0) {
+        notification_bar.innerHTML=("made by MinisGimis#3308")
+      }
+
+      if (lucky_num < 0.025 && lucky_num > 0.015) {
+        var increase = Math.floor(Math.random()*Math.random()*minisgimisPower+1) + 1
+        setminisgimisPower(prevPower => prevPower + increase)
         
         notification_bar.innerHTML=(`you got lucky! MinisGimis' strength increased by ${increase}!`.bold())
-        //console.log("click power increased")
-        setTimeout(() => { notification_bar.innerHTML=("Made by minisgimis#3308")}, 3000);
+        setTime(3)
+        console.log("click power increased")
+        console.log(time)
       }
 
-      if (lucky_num < 0.009) {
-        notification_bar.innerHTML=("you got lucky! MinisGimis doubled!".bold())
-        //console.log("double MinisGimis!")
-        setminisgimis(prevminisgimis => prevminisgimis + prevminisgimis)
-        setTimeout(() => { notification_bar.innerHTML=("Made by MinisGimis#3308")}, 3000);
+      if (lucky_num < 0.015) {
+        var gain = Math.round(Math.random()*100*minisgimisPower)
+        notification_bar.innerHTML=(`You got lucky! A large tree just fell over giving you ${gain} logs!`.bold())
+        console.log("free logs!")
+        setlogs(prevlogs => prevlogs + gain)
+        setTime(3)
+        console.log(time)
       }
 
-    }, 400);
+      if (time > 0) {
+        setTime(prevTime => prevTime -1)
+      }
+
+    }, 1000);
     return () => clearInterval(interval);
-  }, []);
+  }, [time]);
 
   return (
     <div className="App">
       
-        <p id="notification">Made by minisgimis#3308</p>
+        <p id="notification">Made by MinisGimis#3308</p>
         <button className="btn" onClick={
           () => {
             moreInformation()
           }
         }>more information</button>
       
-        <header className="App-header" id="header">{minisgimis}
+        <header className="App-header" id="header">{logs} Logs
         
           <div className="btn-group">
 
             <button onClick={() => {
               minisgimisIt()
-            }}>Get {minisgimisPower} MinisGimis!</button>
+            }}>Chop {minisgimisPower} Trees!</button>
             <button onClick={() => {
               increasePower()
             }}>Feed MinisGimis!</button>
